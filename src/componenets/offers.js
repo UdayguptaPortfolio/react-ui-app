27845ec5371a7ad051a8b1db3d81c401ce47252e
udayongraph <uday.ongraph@gmail.com>
2022-02-21 20:10:05 +0530

    Working on UI

---
+++ src/componenets/offers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/offers.css';
+
+const Offers = () => {
+  return (
+    <div className='offers_container'>
+       <i class="arrow right"></i><h2> WHAT WE <b><strong>OFFER</strong></b></h2><i class="arrow left"></i>
+       <h4>We offer our customers the best services and solutions, this is our main services list</h4>
+       
+      </div>
+  )
+}
+
+export default Offers;
---
+++ src/componenets/upperheader.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../styles/upperheader.css';
+
+const UpperHeader = () => {
+  return (
+    <div className='upperheader_container'>
+    <img className='header_image' src='header_image.jpg' alt='upper header '/>
+    <p>WHAT ARE YOU WAITING FOR?</p>
+    <h1>LET'S BE CREATIVE !</h1>
+    <h2><strong>U</strong>nique,NEXT GENERATION AND HIGHLY FLEXIBLE THEME.</h2>
+    <br/>
+    <i class="arrow right"></i><h3><b><strong>START</strong></b> DOING THAT</h3><i class="arrow left"></i>
+    <br/><br/>
+    <i class="fa fa-chevron-circle-down" 
+    style={{
+        fontSize:'48px',
+        color:'rgb(15, 170, 241)',
+        position: 'absolute',
+        top: '377px',
+    left: '420px'
+        }}></i>
+
+    </div>
+  )
+}
+
+export default UpperHeader;
